9825b7accbe7cd8a0ca06c57afd7f0d212663779
B srikanth reddy <bayagarisrikanthreddy809@gmail.com>
2023-04-11 16:07:55 +0530

    java

---
+++ forloops.js
@@ -0,0 +1,9 @@
+var i=0;
+for (var i=0;i<=5;i++)
+{
+
+
+
+
+                    console.log(i);
+}https://github.com/Bhargava93/javascripthttps://github.com/Bhargava93/javascripthttps://github.com/Bhargava93/javascript
